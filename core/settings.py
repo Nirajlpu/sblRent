896afaeacda6735baa07e22f8f1b02773cf8f284
Niraj Kumar <nirajsahani2004@gmail.com>
2025-08-26 22:29:46 +0530

    debug

--- core/settings.py
+++ core/settings.py
@@ -30,8 +30,8 @@ RAZORPAY_KEY_SECRET = config('RAZORPAY_KEY_SECRET')
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = config('DEBUG', default=False, cast=bool)
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
+# DEBUG = True
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost').split(',')
 TIME_ZONE = 'Asia/Kolkata'
