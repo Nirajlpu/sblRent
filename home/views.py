8e6482deca6dbe51daf732177f3811ebf1a4782b
Niraj Kumar <nirajsahani2004@gmail.com>
2025-08-29 18:05:08 +0530

    improve error message display

--- home/views.py
+++ home/views.py
@@ -494,7 +494,7 @@ def book_property(request, property_id):
     cheek = get_object_or_404(Property, id=property_id)
     if cheek.status != 'active':
         messages.error(request, "Property is not available for booking.")
-        return redirect('dashboard')
+        return redirect('/properties?view=wishlist')
 
     property_obj = get_object_or_404(Property, id=property_id, status='active')
 
